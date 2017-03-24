c31c0f5d2d3ca436577452446205b3bbdd422aec
nutan waikul <nutan waikul>
2017-03-24 23:16:23 +0000

    + ESLint

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
 import chalk  from 'chalk'; // ES06 format to check babel transpiler
 
+/* eslint-disable no-console */
+
 console.log(chalk.green('Starting app in dev mode...'));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './index.css';
 import numalral from 'numeral';
 
+/* eslint-disable no-console */
+
 const coffeeValue = numalral(5).format('$0,0.00');
 console.log(`I love ${coffeeValue} latte!`);
